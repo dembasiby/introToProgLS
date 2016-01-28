acb84fc6891805854a3e46cc5832325612ac9692
dembasiby <dembasiby@gmail.com>
2016-01-28 14:06:03 +0000

    Exo 4 movies in an array

---
+++ 1_the_basics/4-movies_array.rb
@@ -0,0 +1,9 @@
+
+
+movies = [["love_and_death", 1975], ["alexander", 2004], ["gravity", 2013], ["american_pie_2", 2001], ["absence_of_malice", 1981] ]
+
+puts movies[0][1]
+puts movies[1][1]
+puts movies[2][1]
+puts movies[3][1]
+puts movies[4][1]
