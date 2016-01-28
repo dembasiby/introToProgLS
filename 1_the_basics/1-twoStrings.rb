afbb75fb6512fcc42ea5a057f7157bd7e4600645
dembasiby <dembasiby@gmail.com>
2016-01-28 11:36:46 +0000

    Exo 2 modulo & division

---
+++ 1_the_basics/1-twoStrings.rb
@@ -0,0 +1,3 @@
+first_name = "Demba "
+last_name = "Siby"
+name = first_name + last_name
---
+++ 1_the_basics/2-modulo.rb
@@ -0,0 +1,7 @@
+# Use the modulo operator, division, or a combination of both to take a 4 digit number and find 
+# 1) the thousands number 2) the hundreds 3) the tens and 4) and the ones.
+
+puts 7689 / 1000
+puts 7689 % 1000 / 100
+puts 7689 % 100 / 10
+puts 7689 % 10
