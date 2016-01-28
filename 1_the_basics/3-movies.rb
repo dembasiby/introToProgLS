c6e6eb7bbdeb1af2ce09d55112678728e6be225d
dembasiby <dembasiby@gmail.com>
2016-01-28 13:54:29 +0000

    Exo 3 movies

---
+++ 1_the_basics/3-movies.rb
@@ -0,0 +1,12 @@
+movies = {love_and_death: 1975,
+          alexander: 2004,
+          gravity: 2013,
+          american_pie_2: 2001,
+          absence_of_malice: 1981
+}
+
+puts movies[:love_and_death]
+puts movies[:alexander]
+puts movies[:gravity]
+puts movies[:american_pie_2]
+puts movies[:absence_of_malice]
