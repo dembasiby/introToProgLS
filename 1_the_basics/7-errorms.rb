925416b73a8cc387ed169d415fab0221312250f2
dembasiby <dembasiby@gmail.com>
2016-01-30 14:32:25 +0000

    Exo 7 Error message

---
+++ 1_the_basics/7-errorms.rb
@@ -0,0 +1,4 @@
+# SyntaxError: (irb):2: syntax error, unexpected ')', expecting '}'
+# from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+# ==========> A closing curly brace is missing
