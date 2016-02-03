8ad02afd77f3f54a3231c50de3713b9c9a8bdba7
dembasiby <dembasiby@gmail.com>
2016-02-03 23:04:21 +0000

    Complete Arrays section exo

---
+++ 6-Arrays/5-value_of_abc.rb
@@ -0,0 +1,9 @@
+string = "Welcome to America!"
+a = string[6]
+a = "e"
+
+b = string[11]
+b = "A"
+
+c = string[19]
+c = nil # string.length is less than 19, so string[19] does not exist
---
+++ 6-Arrays/6-str_to_i.rb
@@ -0,0 +1,4 @@
+
+
+# Ruby expects integers in the square brackets, not a string. To fix this, 
+# we can just replace 'margaret' by its index, which is 3
